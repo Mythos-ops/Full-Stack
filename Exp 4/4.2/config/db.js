f12abfd92a2d5eb853d6e1088906348636d7cdfa
Harsh Jain <Mythos-ops@users.noreply.github.com>
2026-03-06 21:35:23 +0530

    Added .env

--- Exp 4/4.2/config/db.js
+++ Exp 4/4.2/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-
+require("dotenv").config();
 export const connectDB = async () => {
     try {
-    await mongoose.connect("mongodb+srv://mythosops:harsh070507@exp4.ak9dvio.mongodb.net/?appName=Exp4");
+    await mongoose.connect(process.env.MONGO_URI);;
     console.log("Connected to MongoDB");
     } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
--- Exp 4/4.2/index.js
+++ Exp 4/4.2/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 const app = express();  
+require("dotenv").config();
 
 app.use(express.json());
 connectDB();
