4ab574ab159dfd651590a31d990f64073fd2885a
Harsh Jain <Mythos-ops@users.noreply.github.com>
2026-03-06 22:03:09 +0530

    Added event listener

--- Exp 5/2.1.3/index.js
+++ Exp 5/2.1.3/index.js
@@ -8,6 +8,10 @@ const productRoutes = require('./routes/productRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
